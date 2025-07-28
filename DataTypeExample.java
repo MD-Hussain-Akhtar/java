d13eb0d054d579b7fcdad305cfc6f605e6bea4b4
MD-Hussain-Akhtar <mhussainakhter@gmail.com>
2025-07-28 08:23:54 +0530

    first commit

---
+++ DataTypeExample.java
@@ -0,0 +1,15 @@
+public class DataTypeExample {
+    public static void main(String[] args) {
+        int age = 25;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Hussain Akhtar";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+        System.out.println("Price: $" + price);
+    }
+}
